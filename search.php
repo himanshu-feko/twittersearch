ee473b51420a6044597e3fd8c07991e48d9f2b7f
Himanshu Srivastava <himuvini1990@gmail.com>
2013-03-09 21:29:46 +0530

    Create search.php
    This php code will help to search the twitter tweets about the perticular
    post/entity.

---
+++ search.php
@@ -0,0 +1,10 @@
+<div id="search">
+<form action="" method="get">
+  <label style="color:#FFFFFF">
+  <strong>See what’s happening right now: </strong>
+  <input type="text" name="query" id="searchbox" />
+  <input type="submit" name="submit" id="submit" value="Search" />
+ 
+  </label>
+</form>
+</div>
